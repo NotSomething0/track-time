72e9579178a21a26a0bdd128365743eccbfbcf0e
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2025-11-24 00:35:35 -0600

    feat(auth/login): create login page and component

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -4,9 +4,9 @@ import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
-  const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
+  const payload = await request.json();
+  const email = payload.email;
+  const password = payload.password;
 
   if (!email || !password) {
     return new Response("Email and password are required", { status: 400 });

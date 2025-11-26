b9ee0ba0815287546c3fd5dec336f19e77d240fe
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2025-11-25 18:45:07 -0600

    feat: initial pwa implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,39 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import svelte from '@astrojs/svelte';
+import AstroPWA from '@vite-pwa/astro'
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte()]
+  integrations: [
+    svelte(),
+    AstroPWA({
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', '192.png', '512.png'],
+      manifest: {
+        name: "Track Time",
+        short_name: "TrackTime",
+        description: "Motorsport Tracking",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 });

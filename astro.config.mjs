9b66a0a954f8b8bfdea2dd8024678ef89aee235d
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-01-16 12:41:29 -0600

    feat(astro/config): add node session driver

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 import AstroPWA from '@vite-pwa/astro'
+import node from '@astrojs/node';
+
+const ONE_HOUR = 3600;
 
 export default defineConfig({
+  output: "server",
   vite: {
     plugins: [tailwindcss()]
   },
-
+  adapter: node({
+    mode: 'standalone'
+  }),
+  session: {
+    ttl: ONE_HOUR
+  },
   integrations: [
     svelte(),
     AstroPWA({

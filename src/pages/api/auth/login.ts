0d7c13fbf9cecc7bd5f6ddbecb926bde9b9563af
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-02-26 04:07:03 -0600

    feat(auth/login): read redirect-to header and assign some profile data

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,9 +1,7 @@
-export const prerender = false;
-
 import type { APIRoute } from "astro";
-import { supabase } from "../../../lib/supabase";
+import { getSupabaseClient } from "../../../lib/supabase";
 
-export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+export const POST: APIRoute = async ({ request, session, redirect }) => {
   const payload = await request.json();
   const email = payload.email;
   const password = payload.password;
@@ -12,21 +10,31 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     return new Response("Email and password are required", { status: 400 });
   }
 
-  const { data, error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await getSupabaseClient(session).auth.signInWithPassword({
     email,
     password,
   });
 
   if (error) {
+    console.log(error.message)
     return new Response(error.message, { status: 500 });
   }
 
+  session?.set('profile', { 
+    user_id: data.session.user.id,
+    name: "Unknown"
+  })
+
   const { access_token, refresh_token } = data.session;
-  cookies.set("sb-access-token", access_token, {
-    path: "/",
-  });
-  cookies.set("sb-refresh-token", refresh_token, {
-    path: "/",
-  });
-  return redirect("/dashboard");
+
+  session?.set('sb-access-token', access_token);
+  session?.set('sb-refresh-token', refresh_token);
+  session?.delete('flash');
+
+  const redirectTo = request.headers.get('x-redirect-to');
+
+  if (redirectTo)
+    return redirect(redirectTo);
+
+  return redirect('/dashboard');
 };

590cc69761262f71352c76e6252f1b794fb11e93
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-02-26 03:52:45 -0600

    tweak(lib/supabase): don't auto refresh login token and add additonal logging

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,21 +1,30 @@
-import type { AstroSession } from 'astro';
 import { createClient } from '@supabase/supabase-js';
+import type { AstroSession } from 'astro';
 
-export const getSupabaseClient = (session: AstroSession) => {
+export const getSupabaseClient = (session: AstroSession | undefined) => {
   return createClient(
     import.meta.env.SUPABASE_URL,
     import.meta.env.SUPABASE_PUB_KEY,
     {
       auth: {
+        autoRefreshToken: false,
         storage: {
           getItem: async (key: string) => {
             return await session?.get(key) ?? null;
           },
           setItem: async (key: string, value: string) => {
-            session?.set(key, value);
+            try {
+              session?.set(key, value);
+            } catch {
+              console.log(`Failed to setItem ${key} ${value}`);
+            }
           },
           removeItem: async (key: string) => {
-            session?.set(key, undefined);
+            try {
+              session?.set(key, undefined);
+            } catch {
+              console.log(`Failed to removeItem ${key}`);
+            }
           },
         },
       },

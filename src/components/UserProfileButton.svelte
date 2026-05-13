2dc6fe59ceed938ffb3303fa4d6fa262926f60f7
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-05-13 12:10:09 -0500

    feat(components): Update UserProfileButton/Modal directory and design

---
+++ src/components/UserProfileButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import UserProfileModal from './UserProfileModal.svelte';
+
+  let { profile } = $props();
+
+  let isProfileModalOpen = $state(false);
+
+  function openProfileModal() { isProfileModalOpen = true };
+  function closeProfileModal() { isProfileModalOpen = false };
+
+
+  const avatarUrl = $derived(
+    `https://ui-avatars.com/api/?name=${profile?.username}&size=128`
+  );
+</script>
+
+<button aria-label="Open User Profile Modal" onclick={openProfileModal} class="flex items-center space-x-2">
+  <img src={avatarUrl} alt="Profile" class="w-12 h-12 rounded-md object-cover shadow-lg"/>
+  <span class="text-base text-white/90 font-medium">
+    {profile?.username}
+  </span>
+</button>
+
+<UserProfileModal {profile} {isProfileModalOpen} {closeProfileModal} />

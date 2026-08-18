2fbd52d0abb82d1e4838dcc9f992d5c5bd13ed76
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-08-18 12:54:53 -0500

    chore(astro.config): remove AstroPWA integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
-import AstroPWA from '@vite-pwa/astro'
 import node from '@astrojs/node';
 
 const ONE_HOUR = 3600;
@@ -17,28 +16,5 @@ export default defineConfig({
   session: {
     ttl: ONE_HOUR
   },
-  integrations: [svelte(), AstroPWA({
-    devOptions: {
-      enabled: true
-    },
-    includeAssets: ['favicon.ico', '192.png', '512.png'],
-    manifest: {
-      name: "Track Time",
-      short_name: "TrackTime",
-      description: "Motorsport Tracking",
-      theme_color: "#ffffff",
-      icons: [
-        {
-          src: '192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
-  })]
+  integrations: [svelte()]
 });

81b3c705a81a6c046c26d980000d12fe81a440bf
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-08-23 17:01:14 -0500

    fix(actions): include missing administration action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,11 @@
 import authentication from './authentication';
+import administration from './administration';
 import series from './series';
 import events from './events';
 
 export const server = {
     authentication,
+    administration,
     series,
     events
 };

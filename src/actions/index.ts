7981c556ed533841ab5ff8d7dd2429513759aaa2
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-08-24 13:22:13 -0500

    style(actions/index): format with prettier

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,11 @@
-import authentication from './authentication';
-import administration from './administration';
-import series from './series';
-import events from './events';
+import authentication from "./authentication";
+import administration from "./administration";
+import series from "./series";
+import events from "./events";
 
 export const server = {
-    authentication,
-    administration,
-    series,
-    events
+  authentication,
+  administration,
+  series,
+  events,
 };

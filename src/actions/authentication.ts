899f4ee8c000f54376b17e12bf6c5a3918f8c9da
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-08-25 13:52:15 -0500

    feat(actions/authentication): check if username exists on sign up

--- src/actions/authentication.ts
+++ src/actions/authentication.ts
@@ -1,55 +1,93 @@
-import { defineAction, type ActionAPIContext } from 'astro:actions';
-import { z } from 'astro/zod';
-import { handleAuthError } from '$lib/supabase';
+import {
+  ActionError,
+  defineAction,
+  type ActionAPIContext,
+} from "astro:actions";
+import { z } from "astro/zod";
+import { handleAuthError, handlePostgrestError } from "$lib/supabase";
+import * as db from "../database/profiles";
 
 export default {
-    signUp: defineAction({
-        accept: 'form',
-        input: z.object({
-            email: z.email(),
-            password: z.string()
-        }),
-        handler: async(input: { email: string, password: string }, context: ActionAPIContext) => {
-            const { error } = await context.locals.supabase.auth.signUp({
-                email: input.email,
-                password: input.password
-            })
+  signUp: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.email(),
+      password: z.string(),
+      username: z.string(),
+    }),
+    handler: async (
+      input: { email: string; password: string; username: string },
+      context: ActionAPIContext,
+    ) => {
+      const { data: usernameExists, error: rpcError } =
+        await context.locals.supabase.rpc("check_username_available", {
+          check_username: input.username,
+        });
 
-            if (error) {
-                handleAuthError(error)
-            }
+      if (rpcError) handlePostgrestError(rpcError);
 
-            return { success: true }
+      if (!usernameExists)
+        throw new ActionError({
+          code: "CONFLICT",
+          message: "Username already exists",
+        });
+
+      const { error } = await context.locals.supabase.auth.signUp({
+        email: input.email,
+        password: input.password,
+        options: {
+          data: {
+            username: input.username,
+          },
         },
+      });
+
+      if (error) handleAuthError(error);
+
+      return { success: true };
+    },
+  }),
+  signIn: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.email(),
+      password: z.string(),
     }),
-    signIn: defineAction({
-        accept: 'form',
-        input: z.object({
-            email: z.email(),
-            password: z.string()
-        }),
-        handler: async(input: { email: string , password: string}, context: ActionAPIContext) => {
-            const { error } = await context.locals.supabase.auth.signInWithPassword({
-                email: input.email,
-                password: input.password
-            });
+    handler: async (
+      input: { email: string; password: string },
+      context: ActionAPIContext,
+    ) => {
+      const { data: signInData, error: signInError } =
+        await context.locals.supabase.auth.signInWithPassword({
+          email: input.email,
+          password: input.password,
+        });
 
-            if (error) {
-                handleAuthError(error)
-            }
+      if (signInError) {
+        handleAuthError(signInError);
+      }
 
-            return { success: true }
-        }
-    }),
-    signOut: defineAction({
-        handler: async (_, context: ActionAPIContext) => {
-            const { error } = await context.locals.supabase.auth.signOut();
+      if (signInData.session?.user.id) {
+        const data = await db.getProfileById(
+          context,
+          signInData.session?.user.id,
+        );
 
-            if (error) { 
-                handleAuthError(error)
-            }
+        if (data) context.session?.set("profile", data);
+      }
 
-            return { success: true };
-        }
-    })
-}
+      return { success: true };
+    },
+  }),
+  signOut: defineAction({
+    handler: async (_, context: ActionAPIContext) => {
+      const { error } = await context.locals.supabase.auth.signOut();
+
+      if (error) {
+        handleAuthError(error);
+      }
+
+      return { success: true };
+    },
+  }),
+};

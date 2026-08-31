fe5e9e1b50e4da8043f229a085fd7606fff2b200
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-08-31 18:25:38 -0500

    style(admin/Dashboard): format with prettier

--- src/components/admin/dashboard/Dashboard.svelte
+++ src/components/admin/dashboard/Dashboard.svelte
@@ -7,33 +7,33 @@
 
 <main class="flex h-screen">
   <aside class="bg-[#111111] border border-white/10 w-64">
-      <h1 class="text-white uppercase m-4 text-center">Admin Controls</h1>
-      <nav class="flex flex-col items-center">
-        <button
-          onclick={() => (currentView = "users")}
-          class="flex items-center px-3 py-2.5 mb-1 text-sm font-medium rounded-lg transition-colors
+    <h1 class="text-white uppercase m-4 text-center">Admin Controls</h1>
+    <nav class="flex flex-col items-center">
+      <button
+        onclick={() => (currentView = "users")}
+        class="flex items-center px-3 py-2.5 mb-1 rounded-md cursor-pointer
                     {currentView === 'users'
-            ? 'bg-orange-600 text-white'
-            : 'text-zinc-300 hover:bg-zinc-700 hover:text-white'}"
-        >
-          <span class="mr-2">👤</span> User Manager
-        </button>
+          ? 'bg-orange-600 text-white'
+          : 'text-zinc-300 hover:bg-zinc-700 hover:text-white'}"
+      >
+        <span class="mr-2">👤</span> User Manager
+      </button>
 
-        <button
-          onclick={() => (currentView = "series")}
-          class="flex items-center px-3 py-2.5 text-sm font-medium rounded-lg transition-colors
+      <button
+        onclick={() => (currentView = "series")}
+        class="flex items-center px-3 py-2.5 rounded-md cursor-pointer
                     {currentView === 'series'
-            ? 'bg-orange-600 text-white'
-            : 'text-zinc-300 hover:bg-zinc-700 hover:text-white'}"
-        >
-          <span class="mr-2">🎬</span> Series Manager
-        </button>
-      </nav>
+          ? 'bg-orange-600 text-white'
+          : 'text-zinc-300 hover:bg-zinc-700 hover:text-white'}"
+      >
+        <span class="mr-2">📁</span> Series Manager
+      </button>
+    </nav>
   </aside>
 
-    {#if currentView == "users"}
-      <UserManager />
-    {:else if currentView == "series"}
-      <SeriesManager />
-    {/if}
+  {#if currentView == "users"}
+    <UserManager />
+  {:else if currentView == "series"}
+    <SeriesManager />
+  {/if}
 </main>

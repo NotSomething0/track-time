a209fd0a0a8ebc6297c7dff7882e8b2013c9d981
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-09-06 20:36:59 -0500

    feat(admin/EventManager): add pages to event table

--- src/components/admin/dashboard/manage/event/EventManager.svelte
+++ src/components/admin/dashboard/manage/event/EventManager.svelte
@@ -3,13 +3,23 @@
   import { onMount } from "svelte";
   import type { Events } from "../../../../../database/events";
 
+  const EVENTS_PER_PAGE = 4;
+
   let loading = $state(false);
   let loadingError = $state(false);
   let events: Events = $state([]);
+  let currentPage = $state(0);
+  let totalPages = $derived(Math.ceil(events.length / EVENTS_PER_PAGE));
+  let pagedEvents = $derived(
+    events.slice(
+      currentPage * EVENTS_PER_PAGE,
+      (currentPage + 1) * EVENTS_PER_PAGE,
+    ),
+  );
+
   let searchQuery = $state("");
 
   onMount(async () => {
-    console.log("Mounted event manager");
     loading = true;
 
     const { data, error } = await actions.administration.getAllEvents();
@@ -35,63 +45,94 @@
     class="w-full px-4 py-3 rounded-xl bg-white/5 border border-white/10 text-white placeholder:text-gray-500 mb-4 focus:outline-none"
   />
 
-  <table class="w-full border border-separate border-white/10 rounded-2xl">
-    <thead>
-      <tr class="text-white uppercase font-semibold">
-        <th class="p-4">Name</th>
-        <th class="p-4">Starts</th>
-        <th class="p-4">Ends</th>
-        <th class="p-4">Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#if loading}
-        <tr>
-          <td
-            colspan="4"
-            class="border-t border-white/10 p-3 text-center text-white"
-          >
-            Loading Events...
-          </td>
+  <div class="border border-white/10 rounded-2xl bg-white/2">
+    <table class="w-full">
+      <thead>
+        <tr class="text-white uppercase font-semibold">
+          <th class="p-4">Name</th>
+          <th class="p-4">Track</th>
+          <th class="p-4">Starts</th>
+          <th class="p-4">Actions</th>
         </tr>
-      {:else if loadingError}
-        <tr>
-          <td colspan="4" class="p-3 text-center text-zinc-400">
-            An error occured while loading.
-          </td>
-        </tr>
-      {:else}
-        {#each events as event }
-          <tr class="border-t border-white/5">
+      </thead>
+      <tbody>
+        {#if loading}
+          <tr>
+            <td
+              colspan="4"
+              class="border-t border-white/10 p-3 text-center text-white"
+            >
+              Loading Events...
+            </td>
+          </tr>
+        {:else if loadingError}
+          <tr>
+            <td colspan="4" class="p-3 text-center text-zinc-400">
+              An error occured while loading.
+            </td>
+          </tr>
+        {:else}
+          {#each pagedEvents as pagedEvent}
+            <tr class="border-t border-white/5">
               <th scope="row" class="font-medium text-white">
-                {event.name}
+                {pagedEvent.name}
               </th>
               <td class="text-center">
                 <span
                   class="px-2 py-1 rounded-md bg-[#00A0DE]/10 text-[#00A0DE] border border-[#00A0DE]/20"
                 >
-                  {event.start_at}
+                  {pagedEvent.track ?? "Not set"}
                 </span>
               </td>
-              <td class="text-center text-white">{event.end_at}</td>
+              <td class="text-center text-white"
+                >{new Date(pagedEvent.start_at).getTime()}</td
+              >
               <td class="flex flex-col items-center">
                 <button
-                  onclick={() =>
-                    (console.log("I want to edit", event))}
+                  onclick={() => console.log("I want to edit", pagedEvent)}
                   class="text-white cursor-pointer bg-blue-600 hover:bg-blue-700 rounded-md px-8 py-2 m-2"
                 >
                   Edit
                 </button>
                 <button
-                  onclick={() => console.log("I want to delete", event)}
+                  onclick={() => console.log("I want to delete", pagedEvent)}
                   class="text-white cursor-pointer bg-red-500 hover:bg-red-600 rounded-md px-5 py-2 mb-2"
                 >
                   Delete
                 </button>
               </td>
             </tr>
-        {/each}
-      {/if}
-    </tbody>
-  </table>
+          {/each}
+          {#each Array.from( { length: EVENTS_PER_PAGE - pagedEvents.length } ) as _, i}
+            <tr aria-hidden="true" class="border-t border-white/5">
+              <td colspan="4" class="h-26"></td>
+            </tr>
+          {/each}
+        {/if}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="3" class="border-t border-white/10 text-white p-5">
+            Page {currentPage + 1} of {totalPages}
+          </td>
+          <td class="border-t border-white/10 text-right p-5">
+            <div class="flex justify-end gap-2">
+              <button
+                onclick={() => (currentPage -= 1)}
+                disabled={currentPage < 1}
+                class="px-4 py-2 bg-orange-600 hover:bg-orange-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+                >Previous</button
+              >
+              <button
+                onclick={() => (currentPage += 1)}
+                disabled={currentPage + 1 >= totalPages}
+                class="px-4 py-2 bg-orange-600 hover:bg-orange-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+                >Next</button
+              >
+            </div>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
 </div>
